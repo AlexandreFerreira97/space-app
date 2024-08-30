7fd55cfaae0bca1df3c4818ce5079687dc9700bb
Alexandre <alexandrefaria3012@gmail.com>
2024-08-30 16:46:04 -0300

    Update index.jsx

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,10 +1,18 @@
 import styled from "styled-components"
 
 const ItemDeListaEstilizada = styled.li`
+line-height: 29px;
+font-size: 24px;
+margin-bottom: 30px;
+cursor: pointer;
+color: ${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+display: flex;
+align-items: center;
+gap: 22px;
 `
 
 const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo = false}) => {
-    return <ItemDeListaEstilizada>
+    return <ItemDeListaEstilizada $ativo={ativo}>
         <img src={ativo ? iconeAtivo : iconeInativo} alt="icone" />
         {children}
     </ItemDeListaEstilizada>
