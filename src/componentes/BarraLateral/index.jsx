c0a2135408018e9cccc2053452c637d80dd03030
Alexandre <alexandrefaria3012@gmail.com>
2024-08-30 15:46:38 -0300

    Update index.jsx

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,8 +1,42 @@
+import styled from "styled-components"
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`
+const LinkEstilizado = styled.a`
+    text-decoration: none;
+`
 const BarraLateral = () => {
     return(
-        <>
-        
-        </>
+        <aside>
+            <nav>
+                <ListaEstilizada>
+                    <li>
+                        <LinkEstilizado href="">Início</LinkEstilizado>
+                    </li>
+
+                    <li>
+                        <LinkEstilizado href="">Mais visitas</LinkEstilizado>
+                    </li>
+
+                    <li>
+                        <LinkEstilizado href="">Mais curtidas</LinkEstilizado>
+                    </li>
+
+                    <li>
+                        <LinkEstilizado href="">Novas</LinkEstilizado>
+                    </li>
+
+                    <li>
+                        <LinkEstilizado href="">Surpreenda-me</LinkEstilizado>
+                    </li>
+
+                </ListaEstilizada>
+            </nav>
+        </aside>
     )
 }
 
