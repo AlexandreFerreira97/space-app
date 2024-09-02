ef9d5d4bf2afb85d43e902c0ea31616c55e543d1
Alexandre <alexandrefaria3012@gmail.com>
2024-09-02 19:27:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import EstiloGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/TituloEstilizado"
+import fotoBanner from "../src/assets/banner.png"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -16,6 +18,7 @@ function App() {
       <EstiloGlobais />
       <Cabecalho/>
       <BarraLateral/>
+      <Banner texto={'A galeria mais completa de fotos do espaço'}/>
     </FundoGradiente>
   )
 }
