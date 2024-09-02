1f38bf4b66cbcbd34c56f8068503e0611974e7c9
Alexandre <alexandrefaria3012@gmail.com>
2024-09-02 19:59:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import EstiloGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/TituloEstilizado"
+import Galeria from "./componentes/Galeria"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -19,6 +20,12 @@ const MainContainer = styled.main`
   gap: 24px;
 `
 
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
 function App() {
 
   return (
@@ -28,7 +35,10 @@ function App() {
         <Cabecalho/>
         <MainContainer>
           <BarraLateral/>
-          <Banner texto={'A galeria mais completa de fotos do espaço'}/>
+          <ConteudoGaleria>
+            <Banner texto={'A galeria mais completa de fotos do espaço'}/>
+            <Galeria/>
+          </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
     </FundoGradiente>
