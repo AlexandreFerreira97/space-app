a08f977dc4ee4562c8081777664155c48aa96b13
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 18:29:25 -0300

    Update index.jsx

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,4 +1,12 @@
+import styled from "styled-components"
 import Titulo from "../../Titulo"
+import fotos from "./fotos-populares.json"
+
+const ColunaFotos = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
 
 const Populares = () => {
     return <Titulo $alinhamento="center">Populares</Titulo>
