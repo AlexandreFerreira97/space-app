57be713f161e596ba547dbc1a2dded67f6697c75
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 16:25:33 -0300

    Update index.jsx

---
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+
+const Figure= styled.figure`    
+    width: ${(props) => (props.$expandida ? '90%' :  '460px')};
+    max-width: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    & > img{
+        max-width: 100%;
+        border-radius: 20px 20px 0 0;
+    }
+
+    figcaption{
+        background-color: #001634;
+        border-radius: 0px 0px 20px 20px;
+        color: white;
+        box-sizing: border-box;
+        padding: 12px;
+        h3 {
+            font-family: 'GandhiSansBold';
+        }
+        h4 {
+            flex-grow: 1;
+        }
+        h3,
+        h4 {
+            margin: 0;
+            font-size: 16px;
+        }
+    }
+`
+const Rodape = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Imagem = ({foto, expandida = false}) => {
+    return (
+        <Figure $expandida = {expandida} id={`foto-${foto.id}`}>
+            <img src={foto.path} alt={foto.alt}/>
+            <figcaption>
+                <h3>titulo</h3>
+                <Rodape>
+                    <p>fonte</p>
+                    <button>Favorito</button>
+                    <button>Expandir</button>
+                </Rodape>
+            </figcaption>
+        </Figure>
+    )
+}
+
+export default Imagem
