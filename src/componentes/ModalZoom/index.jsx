123c38f2bbb5821c91295f7d2ae4e03b09eb10d0
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 18:16:30 -0300

    Update index.jsx

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,13 +1,28 @@
-const ModalZoom = ({foto}) => {
+import styled from "styled-components"
+import Imagem from "../Galeria/Imagem"
+
+const Overlay = styled.div`
+    background-color: rgba(0,0,0,0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const ModalZoom = ({ foto }) => {
     return (
-    <>
-        <dialog open={!!foto}>
-            <p>Hi</p>
-            <form method="dialog">
-                <button>OK</button>
-            </form>
-        </dialog>
-    </>)
+        <>
+            {foto && <>
+                <Overlay/>
+                <dialog open={!!foto}>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <button>OK</button>
+                    </form>
+                </dialog>
+            </>}
+        </>)
 }
 
 export default ModalZoom
