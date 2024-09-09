1bc0c7065357a4e9b14ac3ab78b4357f0d423f76
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 17:19:48 -0300

    Update index.jsx

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,6 +1,12 @@
-const ModalZoom = () => {
-    return (<>
-        
+const ModalZoom = ({foto}) => {
+    return (
+    <>
+        <dialog open={!!foto}>
+            <p>Hi</p>
+            <form method="dialog">
+                <button>OK</button>
+            </form>
+        </dialog>
     </>)
 }
 
