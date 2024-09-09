44f03ca971cbf359b42318824fb48a129991c7f8
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 19:03:19 -0300

    Update index.jsx

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -11,7 +11,7 @@ const SecaoFluida = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = ({fotos = [], aoFotoSelecionada}) => {
+const Galeria = ({fotos = [], aoFotoSelecionada, aoAlternarFavorito}) => {
     return (
         <>
             <Tags/>
