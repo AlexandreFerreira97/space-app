7e9d82fb68f33021ada73e6661deb65131b7365f
Alexandre <alexandrefaria3012@gmail.com>
2024-09-09 18:37:43 -0300

    Update index.jsx

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -8,8 +8,33 @@ const ColunaFotos = styled.section`
     gap: 16px;
 `
 
+const Imagem = styled.img`
+    max-width: 212px;
+    border-radius: 20px;
+`
+const Botao = styled.button`
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid;
+    border-color: #C98CF1;
+    padding: 12px 20px;
+    font-size: 20px;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 100%;
+    margin-top: 16px;
+`
+
 const Populares = () => {
-    return <Titulo $alinhamento="center">Populares</Titulo>
+    return (
+        <section>
+            <Titulo $alinhamento="center">Populares</Titulo>
+            <ColunaFotos>
+                {fotos.map(foto => <Imagem key={foto.id} src={foto.path} alt={foto.alt}/>)}
+            </ColunaFotos>
+            <Botao>Ver Mais</Botao>
+        </section>
+    )
 }
 
 export default Populares
